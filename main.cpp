3594643f76ecea8ec5dce1bfcae38f65d403d508
wenke <aideqiaoliang@126.com>
2020-05-10 19:42:36 +0800

    添加css样式的实现方式

--- main.cpp
+++ main.cpp
@@ -17,11 +17,28 @@ int main(int argc, char *argv[])
     translator.load("../QtVideoPro/QtVideoPro_zh_CN.qm");
     a.installTranslator(&translator);
 
+
+    //*******************样式可用qss,也可用css************************************************
     //加载样式文件
     QFile qssFile(":/resourcesFile/StyleSheet.qss");
     qssFile.open(QFile::ReadOnly);
     qApp->setStyleSheet(qssFile.readAll());
     qssFile.close();
+
+#if 0
+    //加载样式表
+    QFile file(":/resourcesFile/StyleSheet.css");
+    if (file.open(QFile::ReadOnly)) {
+        QString qss = QLatin1String(file.readAll());
+        QString paletteColor = qss.mid(20, 7);
+        qApp->setPalette(QPalette(QColor(paletteColor)));
+        qApp->setStyleSheet(qss);
+        file.close();
+    }
+
+#endif
+
+    //*********************************************************************
 
     AppMainWin appInstance;
     appInstance.show();
